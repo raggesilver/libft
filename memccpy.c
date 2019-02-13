de7a161c04244cac7533ccffaa0fdcc97f075551
Paulo Queiroz <pvaqueiroz@gmail.com>
2019-02-13 13:45:25 -0800

    memccpy

--- memccpy.c
+++ memccpy.c
@@ -6,7 +6,7 @@
 /*   By: pqueiroz <pqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 21:10:42 by pqueiroz          #+#    #+#             */
-/*   Updated: 2019/02/12 11:17:29 by pqueiroz         ###   ########.fr       */
+/*   Updated: 2019/02/12 16:52:57 by pqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 {
-	unsigned char *p;
-	const unsigned char *s;
+	unsigned char		*p;
+	const unsigned char	*s;
 
 	p = dest;
 	s = src;
@@ -25,11 +25,10 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	{
 		*p = *s;
 		if (*s == c)
-			break;
+			break ;
 		p++;
 		s++;
 		n--;
 	}
 	return (*p == c) ? p : dest;
 }
-
