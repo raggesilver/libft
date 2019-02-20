3c576548ec124f22b9e8b0429331b4c2fbac8674
Paulo Queiroz <pvaqueiroz@gmail.com>
2019-02-20 11:54:51 -0800

    strlcat fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pqueiroz <pqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 15:40:37 by pqueiroz          #+#    #+#             */
-/*   Updated: 2019/02/18 23:38:37 by pqueiroz         ###   ########.fr       */
+/*   Updated: 2019/02/20 11:54:09 by pqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,19 @@
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
+	char	*tmp;
 	size_t	i;
 	size_t	j;
 
+	tmp = ft_strndup(src, size);
 	i = 0;
 	while (dest[i])
 		i++;
 	j = 0;
-	while (src[j] && j < size)
+	while (tmp[j])
 	{
-		dest[i + j] = src[j];
+		dest[j + i] = tmp[j];
 		j++;
 	}
-	dest[i + j] = 0;
-	i = 0;
-	while (src[i])
-		i++;
 	return (i + size);
 }
