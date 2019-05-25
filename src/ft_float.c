9c50125a1f7f8f4f884fb27e8ed76913095c46a4
Paulo Queiroz <pvaqueiroz@gmail.com>
2019-05-25 00:18:39 -0700

    Initial ft_float actual work

--- src/ft_float.c
+++ src/ft_float.c
@@ -6,7 +6,7 @@
 /*   By: pqueiroz <pqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/13 12:44:37 by pqueiroz          #+#    #+#             */
-/*   Updated: 2019/05/13 17:57:52 by pqueiroz         ###   ########.fr       */
+/*   Updated: 2019/05/24 14:40:38 by pqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,6 @@ t_float		ft_float_new(long double n)
 	t_float res;
 
 	res.sign = (n < 0);
-	res.un = (t_u_ld_i64){.f = n};
-
+	res.dbl.val = n;
 	return (res);
 }
