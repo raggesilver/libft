91e965be786ccaeb09428d71662c4165a74195e9
Paulo Queiroz <pvaqueiroz@gmail.com>
2019-07-08 16:36:23 -0700

    Align argparser with needed by ft_ssl

--- src/argparser/argparser.c
+++ src/argparser/argparser.c
@@ -6,7 +6,7 @@
 /*   By: pqueiroz <pqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 14:09:37 by pqueiroz          #+#    #+#             */
-/*   Updated: 2019/07/08 15:50:16 by pqueiroz         ###   ########.fr       */
+/*   Updated: 2019/07/08 16:24:21 by pqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 static t_arg	*fk_get_arg(t_arg **args, const char *str)
 {
-	size_t		i;
+	ssize_t		i;
 
-	i = 0;
-	while (args[i])
+	i = -1;
+	while (args[++i])
 	{
 		if (ft_strcmp(args[i]->str, str) == 0)
 			return (args[i]);
-		args++;
-		i++;
 	}
 	return (NULL);
 }
@@ -32,18 +30,17 @@ uint32_t		ft_parse_args(t_arg_parser *self)
 	t_arg		*arg;
 
 	self->flags = 0;
-	while (*self->argv)
+	while (*self->argv && **self->argv == '-')
 	{
 		if ((arg = fk_get_arg(self->args, *self->argv)))
 			self->flags |= arg->val;
-		else if (**self->argv != '-' && self->target && !*self->target)
-			*self->target = *self->argv;
-		else if (self->on_extra)
+		else
 		{
-			self->on_extra(self);
+			(self->on_extra) ? self->on_extra(self) : 0;
 			return (0);
 		}
 		self->argv++;
 	}
+	self->target = self->argv;
 	return (self->flags);
 }
