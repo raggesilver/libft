ca2a69d8c1d714c53790205cbb4c2ee6056dfd32
Paulo Queiroz <pvaqueiroz@gmail.com>
2019-07-12 16:38:45 -0700

    Fix memory error on ft_strsplit

--- src/libc/ft_strsplit.c
+++ src/libc/ft_strsplit.c
@@ -6,37 +6,46 @@
 /*   By: pqueiroz <pqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 12:32:46 by pqueiroz          #+#    #+#             */
-/*   Updated: 2019/07/03 21:48:00 by pqueiroz         ###   ########.fr       */
+/*   Updated: 2019/07/12 16:08:55 by pqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-char	**ft_strsplit(const char *s, char c)
+static size_t	fk_get_words(const char *s, char c)
+{
+	size_t	res;
+
+	res = 0;
+	while (*s)
+	{
+		while (*s == c)
+			s++;
+		if (*s)
+			res++;
+		while (*s && *s != c)
+			s++;
+	}
+	return (res);
+}
+
+char			**ft_strsplit(const char *s, char c)
 {
 	char	**res;
-	char	*tmp;
-	size_t	cnt;
+	size_t	i;
 
-	cnt = 1;
-	RETURN_VAL_IF_FAIL(NULL, (s && (res = (char **)malloc(sizeof(*res)))));
-	res[0] = NULL;
-	while (s && *s)
+	res = malloc(sizeof(*res) * (fk_get_words(s, c) + 1));
+	RETURN_VAL_IF_FAIL(NULL, res);
+	i = 0;
+	while (*s)
 	{
-		tmp = ft_strchr(s, c);
-		if (tmp && tmp == s)
-		{
+		while (*s == c)
 			s++;
-			continue ;
-		}
-		RETURN_VAL_IF_FAIL(NULL, (res = ft_realloc(
-			res, sizeof(*res) * (cnt + 1))));
-		res[cnt - 1] = ft_strdupchr(s, c);
-		res[cnt] = NULL;
-		if (tmp)
-			s += ft_strlen(res[cnt++ - 1]);
-		else
-			break ;
+		if (*s)
+			res[i++] = ft_strdupchr(s, c);
+		while (*s && *s != c)
+			s++;
 	}
+	res[i] = NULL;
 	return (res);
 }
