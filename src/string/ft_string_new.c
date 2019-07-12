5266f827e1fb59537c643578c5c0f135bcd9e6b8
Paulo Queiroz <pvaqueiroz@gmail.com>
2019-07-11 18:23:48 -0700

    Fix whatever the hell broke md5, fixes #2

--- src/string/ft_string_new.c
+++ src/string/ft_string_new.c
@@ -6,7 +6,7 @@
 /*   By: pqueiroz <pqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/08 14:19:59 by pqueiroz          #+#    #+#             */
-/*   Updated: 2019/07/09 17:17:35 by pqueiroz         ###   ########.fr       */
+/*   Updated: 2019/07/11 18:22:58 by pqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,15 @@ t_string		*ft_string_new_with_config(const char *s, t_string_cfg cfg)
 	if (s)
 	{
 		self->length = ft_strlen(s);
-		self->size = self->length + 1 + self->cfg.extra;
-		self->data = malloc(self->size);
-		ft_memcpy(self->data, s, self->length + 1);
+		self->size = self->length + self->cfg.extra;
+		self->data = ft_strnew(self->size);
+		ft_memcpy(self->data, s, self->length);
 	}
 	else
 	{
 		self->length = 0;
-		self->size = STRING_GROW_SIZE + 1;
-		self->data = malloc(self->size);
-		*self->data = 0;
+		self->size = STRING_GROW_SIZE;
+		self->data = ft_strnew(self->size);
 	}
 	return (self);
 }
--- src/string/ft_string_new_s.c
+++ src/string/ft_string_new_s.c
@@ -6,7 +6,7 @@
 /*   By: pqueiroz <pqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 17:47:51 by pqueiroz          #+#    #+#             */
-/*   Updated: 2019/07/09 19:39:33 by pqueiroz         ###   ########.fr       */
+/*   Updated: 2019/07/11 18:18:27 by pqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_string		*ft_string_new_s(char **s)
 {
 	t_string *self;
 
+	RETURN_VAL_IF_FAIL(NULL, (s));
 	self = ft_string_new(*s);
 	ft_strdel(s);
 	return (self);
