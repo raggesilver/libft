4e76d09029d4d3d49bb6ee560ccf7835dcad4a40
Paulo Queiroz <pvaqueiroz@gmail.com>
2019-08-20 19:36:01 -0300

    glib based ft_strlen implementation

--- src/libc/ft_strlen.c
+++ src/libc/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: pqueiroz <pqueiroz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/11 21:44:41 by pqueiroz          #+#    #+#             */
-/*   Updated: 2019/07/03 21:48:00 by pqueiroz         ###   ########.fr       */
+/*   Updated: 2019/08/20 19:31:54 by pqueiroz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,24 @@
 
 size_t	ft_strlen(const char *s)
 {
-	size_t res;
+	uint32_t	*p;
+	char		*ptr;
 
-	res = 0;
-	while (*s++)
-		res++;
-	return (res);
+	p = (uint32_t *)s;
+	while (1)
+	{
+		if (((*p) - 0x01010101UL) & ~(*p) & 0x80808080UL)
+		{
+			ptr = (char *)p;
+			if (ptr[0] == 0)
+				return (ptr - s);
+			if (ptr[1] == 0)
+				return (ptr - s + 1);
+			if (ptr[2] == 0)
+				return (ptr - s + 2);
+			if (ptr[3] == 0)
+				return (ptr - s + 3);
+		}
+		p++;
+	}
 }
